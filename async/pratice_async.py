353abd1cc302a32417489b07bf7c2fa03745c7e4
Lucas <cris.matrix02@gmail.com>
2025-05-27 15:22:19 -0300

    Pratice async development

---
+++ async/pratice_async.py
@@ -0,0 +1,29 @@
+import asyncio
+from datetime import datetime
+
+def log(msg):
+    print(f"[{datetime.now().strftime('%H:%M:%S')}] {msg}", flush=True)
+
+async def carregar_cabecalho(nome):
+    log(f'Carregando {nome}')
+    await asyncio.sleep(4)
+    log(f'Terminando {nome}')
+
+async def carregar_conteudo(nome):
+    log(f'Carregando {nome}')
+    await asyncio.sleep(4)
+    log(f'Terminando {nome}')
+
+async def carregar_rodape(nome):
+    log(f'Carregando {nome}')
+    await asyncio.sleep(1)
+    log(f'Terminando {nome}')
+
+async def main():
+    await asyncio.gather(
+        carregar_cabecalho('cabeçalho'),
+        carregar_conteudo('conteúdo'),
+        carregar_rodape('rodapé')
+    )
+
+asyncio.run(main())
